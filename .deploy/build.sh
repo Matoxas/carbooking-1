520bc06f05f788bd5d8b3e97ccfd3852377f63ba
Aidas Eglinskas <programeriss@gmail.com>
2018-11-12 10:53:29 +0200

    'Could not find or access...'

--- .deploy/build.sh
+++ .deploy/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # This script prepares all dependent files to be deployed to server as a single .tar file
+
 # This script intended to be executed in Jenkins CI, but likelly to be extended by you.
 
 set -e # Stop on error
