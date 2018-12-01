aa12d1fc2e8f6e45d5862a2c92daa0d2ecb828c7
Aidas Eglinskas <programeriss@gmail.com>
2018-12-01 22:52:36 +0200

    Edited route for testing...

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -228,4 +228,80 @@ class APIController extends FOSRestController
             Response::HTTP_OK
         );
     }
+
+    /**
+     * @Rest\Post("/newcar", name="api_car_new")
+     * @param Request $request
+     * @return View
+     */
+    public function postNewCarAction(Request $request): View
+    {
+        foreach($_GET as $key => $value) {
+            echo "GET parameter '$key' has '$value' <br/>";
+        }
+
+        foreach($_POST as $key => $value) {
+            echo "POST parameter '$key' has '$value' <br/>";
+        }
+
+        foreach ($_FILES as $FILE) {
+            var_dump($FILE);
+        }
+
+       // var_dump($_FILES['image']);
+        //die;
+/*
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["image"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+        if (isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["image"]["tmp_name"]);
+            if ($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+// Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+// Check file size
+        if ($_FILES["image"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+// Allow certain file formats
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif") {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+// Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+                echo "The file " . basename($_FILES["image"]["name"]) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+*/
+        echo "<br/>";
+
+        return $this->view(
+            [
+                'status' => 'ok',
+                'message' => ''
+            ],
+            Response::HTTP_OK
+        );
+    }
 }
