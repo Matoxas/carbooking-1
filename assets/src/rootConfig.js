80917950c074357f0039fd6ad02129cd6a6a5934
Matas Kazanavičius <matoxas@gmail.com>
2018-12-14 14:50:06 +0200

    hide page

--- assets/src/rootConfig.js
+++ assets/src/rootConfig.js
@@ -1,4 +1,5 @@
-// const baseUrl =
-// const baseUrl = window.location.protocol + "//" + window.location.hostname + "/api/";
-const baseUrl = "https://carbooking.projektai.nfqakademija.lt/api/";
+
+// const baseUrl = "https://carbooking.projektai.nfqakademija.lt/api/";
+const baseUrl = window.location.protocol + "//" + window.location.hostname + "/api/";
+export const dontShowPage = true;
 export default baseUrl;
