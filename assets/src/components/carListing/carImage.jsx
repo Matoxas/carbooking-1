57e067dce38cf221f64b397c547052d6aa257ab3
Adomas Poleninas <adomas.poleninas@gmail.com>
2018-12-16 22:03:58 +0200

    fix on mobile picture slider

--- assets/src/components/carListing/carImage.jsx
+++ assets/src/components/carListing/carImage.jsx
@@ -17,11 +17,11 @@ class carImage extends Component {
                     })}
                 </div>
                 <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="carousel-control-prev-icon" aria-hidden="true"/>
                     <span className="sr-only">Previous</span>
                 </a>
                 <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="carousel-control-next-icon" aria-hidden="true"/>
                     <span className="sr-only">Next</span>
                 </a>
             </div>
