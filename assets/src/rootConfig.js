193d2c687bc5ce2527511794aef7bb2ea03239d8
Adomas Poleninas <adomas.poleninas@gmail.com>
2018-12-16 18:45:16 +0200

    merge

--- assets/src/rootConfig.js
+++ assets/src/rootConfig.js
@@ -1,11 +1,4 @@
-
-<<<<<<< HEAD
-const baseUrl = "https://carbooking.projektai.nfqakademija.lt/api/";
-// const baseUrl = window.location.protocol + "//" + window.location.hostname + "/api/";
-export const dontShowPage = false;
-=======
 //const baseUrl = "https://carbooking.projektai.nfqakademija.lt/api/";
 const baseUrl = window.location.protocol + "//" + window.location.hostname + "/api/";
-export const dontShowPage = true;
->>>>>>> ef4e674f4de3d310ad113f8252e28439235e30e0
+export const dontShowPage = false;
 export default baseUrl;
