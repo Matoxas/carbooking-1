1ead9d37ec81cb59a2c5cb13ccf69bd6d4822fe3
Matas Kazanavičius <matoxas@gmail.com>
2018-12-17 16:49:21 +0200

    route updated

--- assets/src/rootConfig.js
+++ assets/src/rootConfig.js
@@ -1,11 +1,5 @@
-{/*<<<<<<< HEAD*/}
-{/*//const baseUrl = "https://carbooking.projektai.nfqakademija.lt/api/";*/}
-{/*const baseUrl = window.location.protocol + "//" + window.location.hostname + "/api/";*/}
-{/*export const dontShowPage = false;*/}
-{/*export default baseUrl;*/}
-{/*=======*/}
 
-const baseUrl = "https://carbooking.projektai.nfqakademija.lt/api/";
-// const baseUrl = window.location.protocol + "//" + window.location.hostname + "/api/";
+// const baseUrl = "https://carbooking.projektai.nfqakademija.lt/api/";
+const baseUrl = window.location.protocol + "//" + window.location.hostname + "/api/";
 export const dontShowPage = false;
 export default baseUrl;
