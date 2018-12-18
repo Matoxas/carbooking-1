6395a234f08caeddbeda1a546bf12d307fa45c63
Matas Kazanavičius <matoxas@gmail.com>
2018-12-18 18:04:04 +0200

    added page for noFavourites selected

--- assets/src/components/favourites/Favourites.jsx
+++ assets/src/components/favourites/Favourites.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import { inject, observer } from "mobx-react";
-import Item from "./item";
+import Item from "../feed/item";
+import NoFavsWrapper from "./noFavs/noFavsWrapper";
 
 @inject("CarStore")
 @observer
 class Favourites extends Component {
   render() {
     const { likedCars: cars } = this.props.CarStore;
+
+    if (cars.length == 0) {
+      return <NoFavsWrapper />;
+    }
+
     return (
       <div className="container">
         <h2>Jūsų mėgstami automobiliai</h2>
